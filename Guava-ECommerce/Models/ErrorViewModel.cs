30acc3e31ba972142741f239a1b00e9eb47d7093
Elysian Atlys <31394611+alyssaolivia@users.noreply.github.com>
2023-09-01 20:39:06 -0400

    Add project files.

---
+++ Guava-ECommerce/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Guava_ECommerce.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
